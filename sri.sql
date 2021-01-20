f4cc32a51671fb008201f3a86e7ec0317664e12a
madeblanka <38938512+madeblanka@users.noreply.github.com>
2021-01-20 15:06:37 +0800

    Add files via upload

---
+++ sri.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 20, 2021 at 08:05 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sri`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banjar`
+--
+
+CREATE TABLE `banjar` (
+  `id` int(11) NOT NULL,
+  `periode_Id` int(11) NOT NULL,
+  `nama_stt` varchar(20) NOT NULL,
+  `nama_ket_stt` varchar(30) NOT NULL,
+  `nama_banjar` varchar(50) NOT NULL,
+  `gambar` varchar(20) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `created_at` varchar(25) NOT NULL,
+  `updated_at` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `juri`
+--
+
+CREATE TABLE `juri` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `created_at` varchar(25) NOT NULL,
+  `updated_at` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `id` int(11) NOT NULL,
+  `banjar_id` int(11) NOT NULL,
+  `juri_id` int(11) NOT NULL,
+  `wujud` float NOT NULL,
+  `keutuhan` float NOT NULL,
+  `bahan` float NOT NULL,
+  `tinggi` float NOT NULL,
+  `unsur` float NOT NULL,
+  `ekspresi` float NOT NULL,
+  `keindahan` float NOT NULL,
+  `keserasian` float NOT NULL,
+  `inovasi` float NOT NULL,
+  `created_at` varchar(25) NOT NULL,
+  `updated_at` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `periode`
+--
+
+CREATE TABLE `periode` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `juri_id` int(11) NOT NULL,
+  `periode` varchar(50) NOT NULL,
+  `tgl_pelaksanaan` varchar(15) NOT NULL,
+  `tahun` varchar(10) NOT NULL,
+  `keterangan` text NOT NULL,
+  `status` text NOT NULL,
+  `created_at` varchar(25) NOT NULL,
+  `updated_at` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `alamat` text NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banjar`
+--
+ALTER TABLE `banjar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `juri`
+--
+ALTER TABLE `juri`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `periode`
+--
+ALTER TABLE `periode`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banjar`
+--
+ALTER TABLE `banjar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `juri`
+--
+ALTER TABLE `juri`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nilai`
+--
+ALTER TABLE `nilai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `periode`
+--
+ALTER TABLE `periode`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
